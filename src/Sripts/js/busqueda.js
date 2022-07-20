8808ea9fc275a60ac13c6b71818cd1a400359ca4
Gsuscrist <100723141+Gsuscrist@users.noreply.github.com>
2022-07-20 08:51:41 -0500

    Add files via upload

---
+++ src/Sripts/js/busqueda.js
@@ -0,0 +1,278 @@
+
+const conexion = require('./conexion');
+
+class producto{
+    constructor( id, imagen, modelo, medida, rC, rV, uso, cantidad, precio){
+        this.id=id;
+        this.imagen=imagen;
+        this.modelo=modelo;
+        this.medida=medida;
+        this.rC=rC;
+        this.rV=rV;
+        this.uso=uso;
+        this.cantidad=cantidad;
+        this.precio=precio;
+    }
+}
+
+class Nodo{
+    constructor(llave, obj){
+        this.llave = llave
+        this.obj = obj
+
+        this.der = null
+        this.izq = null
+    }
+}
+
+
+class Tree {
+    constructor() {
+        this.raiz = null
+    }
+
+    isEmpty() {
+        return this.raiz === null
+    }
+
+    Agregar(llave, obj) {
+        // arbol no tiene elementos
+        if (this.isEmpty()) {
+            this.raiz = new Nodo(llave, obj);
+            return;
+        }
+
+        var aux = this.raiz;
+
+        while (aux) {
+            // vamos hacia la izquierda
+            if (llave < aux.llave) {
+                if (aux.izq) {
+                    aux = aux.izq;
+                } else {
+                    aux.izq = new Nodo(llave, obj);
+                    return;
+                }
+            } else { // vamos hacia la derecha
+                if (aux.der) {
+                    aux = aux.der;
+                } else {
+                    aux.der = new Nodo(llave, obj);
+                    return;
+                }
+            }
+        }
+    }
+
+
+
+    Buscar(llave) { //! funcion de busqueda
+        if (this.isEmpty()) {
+            return null
+        }
+
+        var aux = this.raiz
+        if (aux.llave === llave) {
+            return aux
+        }
+
+        while (aux) {
+            // si encontramos el nodo con el valor
+            // paramos de iterar.
+            if (aux.llave === llave) {
+                break
+            }
+            // seguimos buscando a la derecha
+            if (aux.llave < llave) {
+                aux = aux.der
+            } else if (aux.llave > llave) {
+                // seguimos buscando a la izquierda
+                aux = aux.izq
+            }
+        }
+        // retornamos el nodo encontrado.
+        // si no encontramos el nodo con el valor
+        // aux, toma el valor null.
+        return aux
+    }
+
+    Encontrando(Nodo = this.raiz) { //* funcion que ayuda a delete()
+        if (!this.isEmpty()) {
+            /**
+             * siempre a la izquierda de cualquier nodo
+             * estará el menor valor.
+             * iteramos hasta el último menor.
+             */
+            while (Nodo.izq) {
+                Nodo = Nodo.izq
+            }
+            return Nodo
+        }
+    }
+
+    Borrar(llave, Nodo = this.raiz) { //! funcion de eliminar
+        if (!Nodo) {
+            return null
+        }
+        if (Nodo.llave === llave) {
+            // no tiene hijos
+            if (!Nodo.izq && !Nodo.der) {
+                return null
+            }
+            // no tiene hijo izquierdo
+            if (!Nodo.izq) {
+                return Nodo.der
+            }
+            // no tiene hijo derecho
+            if (!Nodo.der) {
+                return Nodo.izq
+            }
+
+            // tiene dos hijos
+            // buscamos el menor de los hijos
+            var temp = this.Encontrando(Nodo.der)
+            // con ese valor reemplazamos el valor del nodo que queremos eliminar.
+            Nodo.llave = temp.llave;
+            // seguimos iterando para reemplazar la rama que cambio,
+            // eliminando el nodo que está repetido
+            Nodo.der = this.Borrar(temp.llave, Nodo.der)
+            return Nodo;
+        }
+        // buscamos a la derecha
+        if (Nodo.llave < llave) {
+            Nodo.der = this.Borrar(llave, Nodo.der)
+            return Nodo
+        }
+        // buscamos a la izquierda
+        if (Nodo.llave > llave) {
+            Nodo.izq = this.Borrar(llave, Nodo.izq)
+            return Nodo
+        }
+    }
+ //! funciones para la imprension en consola
+ print(Nodo = this.raiz) {
+    if (!Nodo) {
+        return
+    }
+    this.print(Nodo.izq)
+    console.log('llave: ' + Nodo.llave + '\n\t', Nodo.obj)
+    this.print(Nodo.der)
+}
+/**
+ * recorre primero toda la rama izquierda
+ * de izquierda al centro.
+ * Luego imprime la raíz, y finalmente
+ * recorre la rama derecha, del centro hacia
+ * la derecha.
+ */
+inOrder(Nodo = this.raiz) {
+    if (!Nodo) {
+        return;
+    }
+    this.inOrder(Nodo.izq);
+    console.log('llave: ' + Nodo.llave + '\n\t', Nodo.obj);
+    datos.push(Nodo);
+    imagen.push(Nodo.obj.imagen);
+    id.push(Nodo.obj.id);
+    marca.push(Nodo.llave);
+    modelo.push(Nodo.obj.modelo);
+    medida.push(Nodo.obj.medida);
+    rango_carga.push(Nodo.obj.rC);
+    rango_vel.push(Nodo.obj.rV);
+    uso.push(Nodo.obj.uso);
+    precio.push(Nodo.obj.precio);
+    cantidad.push(Nodo.obj.cantidad);
+
+
+    this.inOrder(Nodo.der);
+}
+
+}
+
+
+var tree =new Tree();
+
+let datos=[];
+let imagen=[];
+let id=[];
+let marca=[];
+let modelo=[];
+var medida=[];
+let rango_carga=[];
+let rango_vel=[];
+let uso=[];
+let precio=[];
+let cantidad=[];
+
+function busqueda(){
+    //accion de limpieza de tabla
+    const $elemento = document.querySelector("#table");
+    $elemento.innerHTML=`<table class="table" id="table">
+    <th class="column" id="imagen"></th>
+    <th class="column" id="marca">Marca</th>
+    <th class="column" id="modelo">Modelo</th>
+    <th class="column" id="medida">Medida</th>
+    <th class="column" id="cw">Rango de Carga</th>
+    <th class="column" id="cv">Rango de Velocidad</th>
+    <th class="column" id="uso">Tipo de Uso</th>
+    <th class="column" id="precio">Precio</th>
+    <th class="column" id="cant"> Cantidad</th>
+</table>`;
+    //metodo de busqueda y organizacion
+    var medida = document.getElementById("buscar").value;
+    $query = `select *from llanta where medida ='${medida}';`
+    let tablaR = document.getElementById("table");
+
+    conexion.query($query, function (err, rows) {
+
+        if (err) {
+            console.log("error en el query");
+            console.log(err);
+            return;
+        }else{
+            if(rows.length===0){
+                alert("Producto no encontrado")
+            }else{
+                for(i=0;i<rows.length;i++){
+                    tree.Agregar(rows[i].marca, new producto(rows[i].id_llanta, rows[i].imagen, rows[i].modelo, rows[i].medida, rows[i].rango_carga, rows[i].rango_vel, rows[i].uso, rows[i].cantidad, rows[i].precio))
+                }
+                tree.inOrder();
+                for(i=0;i<datos.length;i++){
+                    //aca creas las columnas
+                    var newRow = tablaR.insertRow(-1);
+                    var celdaimg = newRow.insertCell(0)
+                    var celdamarca = newRow.insertCell(1);
+                    var celdamodelo = newRow.insertCell(2);
+                    var celdamedida = newRow.insertCell(3);
+                    var celdarangoW = newRow.insertCell(4);
+                    var celdarangoV = newRow.insertCell(5);
+                    var celdauso = newRow.insertCell(6);
+                    var celdaprecio = newRow.insertCell(7);
+                    var celdacant = newRow.insertCell(8);
+                    //aca dices la informacion
+                    var txtimg = `<img class="size" src="${imagen[i]}">`;
+                    var txtmarca = document.createTextNode(marca[i]);
+                    var txtmodelo = document.createTextNode(modelo[i]);
+                    var txtmedida = document.createTextNode(medida[i]);
+                    var txtrangoW = document.createTextNode(rango_carga[i]);
+                    var txtrangoV = document.createTextNode(rango_vel[i]);
+                    var txtuso = document.createTextNode(uso[i]);
+                    var txtprecio = document.createTextNode(precio[i]);
+                    var txtcant = document.createTextNode(cantidad[i]);
+                    //aca dices que poner y donde
+                    celdaimg.innerHTML+=txtimg;
+                celdamarca.appendChild(txtmarca);
+                celdamodelo.appendChild(txtmodelo);
+                celdamedida.appendChild(txtmedida);
+                celdarangoW.appendChild(txtrangoW);
+                celdarangoV.appendChild(txtrangoV);
+                celdauso.appendChild(txtuso);
+                celdaprecio.appendChild(txtprecio);
+                celdacant.appendChild(txtcant);
+                }
+
+            }
+
+        }
+    });
+}
---
+++ src/Sripts/js/home.js
@@ -0,0 +1,78 @@
+const conexion = require('./conexion');
+
+
+
+
+$query = 'select *from llanta;';
+conexion.query($query, function (err, rows) {
+    if (err) {
+    console.log("error en el query");
+    console.log(err);
+    return;
+    }
+    else{
+    //Lo que se extrae de la BD, queda guardado en ROWS que se vuelve una lista de objetos  
+   
+for(i=1;i<=10;i++){
+
+    let content = `  
+    
+    <a>
+    <div class="div${i}">
+    <div> <img class="size" src="${rows[i-1].imagen}"> </div>
+    <div><b><label id="tagMarca" value="${rows[i-1].marca}">${rows[i-1].marca}</label><br>
+    
+    <label id="tagMedida" value="${rows[i-1].medida}">${rows[i-1].medida}</label></b>
+    <br>
+    <br>
+    ${"Marca: " +rows[i-1].marca +"\n"+
+    "Medida: " + rows[i-1].medida+"\n"+
+    "Modelo: " +rows[i-1].modelo+"\n"+
+    "Rango de Carga: "+rows[i-1].rango_carga+"\n"+
+    "Rango de Velocidad: "+rows[i-1].rango_vel+"\n"
+}
+<br>
+<div>
+<button>${"$"+ rows[i-1].precio}</button><br>
+<button class="verID" id="bttnver" value="${rows[i-1].id_llanta}">Ver</button>
+</div>   
+</div>
+    </div>
+   </a> ` ;
+    
+    list.innerHTML += content;
+}
+
+//prueba
+var botones=document.querySelectorAll('.verID')
+    botones.forEach(el=>{
+        el.addEventListener('click',(e)=>{
+           
+            console.log(e.target.value);
+            localStorage.setItem("product",JSON.stringify(e.target.value));
+
+            window.location.href="product.html"
+        });
+    });
+
+    }
+    });
+
+
+
+
+
+/** 
+function ver(){
+    let producto;
+   
+
+    producto = id_llanta.textContent;
+
+        localStorage.setItem("product",JSON.stringify( producto));
+        alert(JSON.parse( localStorage.getItem("product")));
+        localStorage.clear();
+    }
+*/
+
+
